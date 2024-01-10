7beb3c981f0c1df97d33031124e5df712a4680f9
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-01-10 18:22:30 +0530

    Update Footer Template

--- footer.php
+++ footer.php
@@ -12,22 +12,45 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-
-		<hr>
+		<div class="footer-container">
+			<hr style="border: 1px solid #62585f; margin-top: 20px">
+			<div class="footer-text">
+				<div>
+					<p class="headliner-text" style="font-size: 28px;">Welcome to D'SIGN<em>fly</em></p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio doloribus ducimus esse et expedita facere illo illum inventore ipsa ipsam, minus natus neque, quaerat quas quasi repellendus sapiente ullam voluptatum.</p>
+					<a href="#">Read More</a>
+				</div>
+				<div style="width: 100%">
+					<p class="headliner-text" style="font-size: 28px;">Contact Us</p>
+					<p>Street 21 Planet, A-11, dapibus tristique 123511<br>
+						Tel:123 456 7890 Fax:123 456789<br>
+						Email: <a href="mailto:contactus@dsignfly.com">contactus@dsignfly.com</a></p>
+					<div class="socials">
+						<a href="#"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/facebook.png" alt="facebook"></a>
+						<a href="#"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/g+.png" alt="twitter"></a>
+						<a href="#"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/linkedin.png" alt="instagram"></a>
+						<a href="#"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/pin.png" alt="pinterest"></a>
+						<a href="#"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/oldx.png" alt="youtube"></a>
+					</div>
+				</div>
+			</div>
+			<hr style="border: 1px solid #62585f;margin-top: 30px">
+		</div>
 		<div class="site-info">
 			<span>
 				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( '© 2021 - %1$s', 'dsignfly' ), 'D`SIGNfly' );
 				?>
 			</span>
 
 			<span class="sep"> | </span>
 				<?php
-
+				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Designed by %1$s.', 'dsignfly' ), '<a href="https://rtcamp.com/">rtCamp</a>' );
 				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		</div>
+	</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
