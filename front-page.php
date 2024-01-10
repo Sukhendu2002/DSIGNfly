c76ce70f2feed19754ae3797d98d460c6bb83084
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-01-10 16:43:52 +0530

    Add Feature Template Part

--- front-page.php
+++ front-page.php
@@ -12,13 +12,14 @@ get_header();
 		<div class="slider">
 			<div class="slider-text"><p>Gearing up the ideas</p></div>
 			<div class="slider-arrow1">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-right.png" alt="arrow">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/arrow-right.png" alt="arrow">
 			</div>
 			<div class="slider-arrow2">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-left.png" alt="arrow">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/arrow-left.png" alt="arrow">
 			</div>
 			<div class="slider-text2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eaque et iusto natus nostrum omnis quia quo.</div>
 		</div>
+		<?php get_template_part( 'template-parts/features' ); ?>
 	</div>
 
 <?php
---
+++ template-parts/features.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying the Features.
+ *
+ * @package Dsignfly
+ */
+?>
+<div class="main-features">
+<div class="features-container">
+			<div class="feature">
+				<div class="feature__icon">
+					<img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/feature-leaf.png" alt="">
+				</div>
+				<div class="feature__info">
+					<h3 class="feature__title">Advertising</h3>
+					<p class="feature__description">Lorem ipsum dolor sit amet, consectetur. </p>
+				</div>
+			</div>
+			<div class="feature">
+				<div class="feature__icon">
+					<img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/feature-mobile.png" alt="">
+				</div>
+				<div class="feature__info">
+					<h3 class="feature__title">Multimedia</h3>
+					<p class="feature__description">Lorem ipsum dolor sit amet, consectetur. </p>
+				</div>
+			</div>
+			<div class="feature">
+				<div class="feature__icon">
+					<img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/feature-camera.png" alt="">
+				</div>
+				<div class="feature__info">
+					<h3 class="feature__title">Photography</h3>
+					<p class="feature__description">Lorem ipsum dolor sit amet, consectetur.</p>
+				</div>
+			</div>
+</div>
+</div>
